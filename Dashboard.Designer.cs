278da21a672c47f1991a88b21915a3d93998451c
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-06-25 19:16:39 +0530

    edit

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -276,7 +276,7 @@
             this.Name = "Dashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
-            this.Load += new System.EventHandler(this.Dashboard_Load);
+            //this.Load += new System.EventHandler(this.Dashboard_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
