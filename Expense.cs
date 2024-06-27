597c786e3f820ed3116efaeadacb416cae79dbd4
preethesh07 <preethu07y@gmail.com>
2024-06-27 14:15:58 +0530

    Expenses Form updated

--- Expense.cs
+++ Expense.cs
@@ -26,5 +26,15 @@ namespace BugetBee_Expenses_Tracker
         {
 
         }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
