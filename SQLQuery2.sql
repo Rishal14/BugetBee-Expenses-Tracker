8157920e167f27490525f50d611b3bbc6f2d45cd
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-06-28 22:02:36 +0530

    updated query

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE users(
-id INT PRIMARY KEY,
-   name VARCHAR(255),
-  email VARCHAR(255),
-  password VARCHAR(255)
+﻿CREATE TABLE users (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    username VARCHAR(MAX) NULL,
+    password VARCHAR(MAX) NULL,
+    date_created DATE NULL
 );
